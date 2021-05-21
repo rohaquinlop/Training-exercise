6ea9624d89c2ad524568c394912b3aeeb9a0161f
fexxel <rohaquinlop301@gmail.com>
2021-05-20 23:33:19 -0500

    Web setup

--- README.md
+++ README.md
@@ -2,9 +2,15 @@
 
 ## GraphDB
 
-
 start server
 ```bash
 cd backend
 go run server.go
 ```
+
+Buyers endpoint:
+```
+https://kqxty15mpg.execute-api.us-east-1.amazonaws.com/buyers?date=$date
+$date unix timestamp format, converter, hours don't matter
+```
+
--- backend/server.go
+++ backend/server.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/go-chi/chi/v5"
@@ -14,6 +16,17 @@ import (
 
 const defaultPort = "8080"
 
+func getActualDateTime(value string) string {
+	if value == "" {
+		date := fmt.Sprintf("%d", time.Now().Unix())
+		return date
+	} else {
+		tmp, _ := time.Parse(time.RFC3339, value)
+		date := fmt.Sprintf("%d", tmp.Unix())
+		return date
+	}
+}
+
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
@@ -22,13 +35,21 @@ func main() {
 		port = defaultPort
 	}
 
+	localDate := getActualDateTime("")
+
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
 	//http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	frontDir := http.Dir("../frontend")
-	http.Handle("/", http.FileServer(frontDir))
+
+	//Frontend files
+	rootDir := http.Dir("../frontend/")
+	http.Handle("/", http.FileServer(rootDir))
+
+	//graphql server
 	http.Handle("/graphql", srv)
 
-	log.Printf("connect to http://localhost:%s/", port)
+	log.Printf("local unix timestamp: %s\n", localDate)
+
+	log.Printf("connect to http://localhost:%s/home", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
