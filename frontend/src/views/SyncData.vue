87699abbd5d8f582c4adf2b5267cec812310f503
fexxel <rohaquinlop301@gmail.com>
2021-05-29 21:26:40 -0500

    three dots loading screen

--- frontend/src/views/SyncData.vue
+++ frontend/src/views/SyncData.vue
@@ -1,50 +1,57 @@
 <template>
-  <div class="syncdata">
-    <h1>Sincronizar datos</h1>
-      <v-menu
-        ref="menu"
-        v-model="menu"
-        :close-on-content-click="false"
-        :return-value.sync="date"
-        transition="scale-transition"
-        offset-y
-        min-width="auto"
-      >
-        <template v-slot:activator="{ on, attrs }">
-          <v-text-field
-            v-model="date"
-            label="Selecciona la fecha a sincronizar"
-            prepend-icon="mdi-calendar"
-            readonly
-            v-bind="attrs"
-            v-on="on"
-          ></v-text-field>
-        </template>
-        <v-date-picker
-          v-model="date"
-          no-title
-          scrollable
+  <div class="top">
+    <div class="syncdata">
+      <h1>Sincronizar datos</h1>
+        <v-menu
+          ref="menu"
+          v-model="menu"
+          :close-on-content-click="false"
+          :return-value.sync="date"
+          transition="scale-transition"
+          offset-y
+          min-width="auto"
         >
-          <v-spacer></v-spacer>
-          <v-btn
-            text
-            color="primary"
-            @click="menu = false"
+          <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+              v-model="date"
+              label="Selecciona la fecha a sincronizar"
+              prepend-icon="mdi-calendar"
+              readonly
+              v-bind="attrs"
+              v-on="on"
+            ></v-text-field>
+          </template>
+          <v-date-picker
+            v-model="date"
+            no-title
+            scrollable
           >
-            Cancel
-          </v-btn>
-          <v-btn
-            text
-            color="primary"
-            @click="$refs.menu.save(date)"
-          >
-            OK
-          </v-btn>
-        </v-date-picker>
-      </v-menu>
-      <v-row aling="center" @click="loadData">
-        <v-btn dark color="blue">Sincronizar</v-btn>
-      </v-row>
+            <v-spacer></v-spacer>
+            <v-btn
+              text
+              color="primary"
+              @click="menu = false"
+            >
+              Cancel
+            </v-btn>
+            <v-btn
+              text
+              color="primary"
+              @click="$refs.menu.save(date)"
+            >
+              OK
+            </v-btn>
+          </v-date-picker>
+        </v-menu>
+        <div v-if="!disabled">
+          <v-row aling="center" @click="loadData" :disabled="disabled">
+            <v-btn dark color="blue">Sincronizar</v-btn>
+          </v-row>
+        </div>
+        <div v-else class="loading">
+          Cargando los datos
+        </div>
+    </div>
   </div>
 </template>
 
@@ -58,12 +65,62 @@ export default {
     return{
       date: new Date().toISOString().substr(0, 10),
       menu: false,
+      disabled: false,
+      timeout: null,
     }
   },
   methods: {
-    loadData(){
+    loadData() {
       fetch('http://localhost:5656/load'.concat('/',this.date), {method: 'POST', mode:'no-cors'})
-    }
+      this.disabled = true
+
+      this.timeout = setTimeout(() => {
+        this.disabled = false
+      }, 9000)
+    },
   },
+  beforeDestroy() {
+    clearTimeout(this.timeout)
+  }
 }
 </script>
+
+<style>
+
+.top{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80vh;
+}
+
+.syncdata{
+  margin: auto;
+  width: 50vw;
+  padding: 10px;
+  align-items: center;
+}
+
+.loading:after{
+  overflow: hidden;
+  display: inline-block;
+  vertical-align: bottom;
+  -webkit-animation: ellipsis steps(4, end) 900ms infinite;
+  animation: ellipsis steps(4, end) 900ms infinite;
+  content: "\2026";
+  width: 0px;
+}
+
+@keyframes ellipsis{
+  to{
+    width: 20px;
+  }
+}
+
+@-webkit-keyframes ellipsis{
+  to {
+    width: 20px;
+  }
+}
+
+</style>
