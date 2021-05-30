27c28c2a40e3b4eb309832b6550aeb36f20839d9
fexxel <rohaquinlop301@gmail.com>
2021-05-29 23:42:01 -0500

    buyers view

--- frontend/src/views/Buyers.vue
+++ frontend/src/views/Buyers.vue
@@ -1,16 +1,27 @@
 <template>
-  <div class="buyers">
-    <h1>Compradores</h1>
+  <div class="top">
+    <div class="buyers">
+      <h1>Compradores</h1>
+      <v-divider></v-divider>
 
-    <div v-if="buyers">
-      <ul>
-      <li v-for="buyer in buyers" :key="buyer.id">
-        {{buyer.id}} {{buyer.name}}
-      </li>
-    </ul>
-    </div>
-    <div vi-else>
-      <p>No se han sincronizado los datos</p>
+      <div v-if="buyersloaded" class="scrollable-list">
+        <table class="buyers-table">
+          <tr class="buyers-tr">
+            <th class="buyers-th">ID</th>
+            <th class="buyers-th">Nombre</th>
+            <th class="buyers-th">Edad</th>
+          </tr>
+          <tr v-for="buyer in buyers" :key="buyer.id" class="buyers-tr">
+            <td class="buyers-td">{{buyer.id}}</td>
+            <td class="buyers-td">{{buyer.name}}</td>
+            <td class="buyers-td">{{buyer.age}}</td>
+          </tr>
+        </table>
+      </div>
+      <div v-else>
+        <h2>No se han sincronizado fechas</h2>
+        <a href="/">Sincronizar fecha</a>
+      </div>
     </div>
   </div>
 </template>
@@ -19,7 +30,7 @@
 export default {
   data() {
     return{
-      buyersLoaded: false,
+      buyersloaded: false,
       buyers: null,
     }
   },
@@ -30,18 +41,61 @@ export default {
     '$route': 'fetchBuyers'
   },
   methods: {
-    fetchBuyers() {
-      this.buyers = null
+    async fetchBuyers(){
       fetch('http://localhost:5656/buyers', {method: 'GET'}).then(response => response.json()).then(json => {
         //remove duplicated
         this.buyers = json.filter( (json, id, self) =>
           id === self.findIndex((t) => (t.id === json.id))
         );
         if(this.buyers.length > 0){
-          this.buyersLoaded = true;
+          this.buyersloaded = true;
         }
       })
     },
   }
 }
 </script>
+
+<style>
+
+.top{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80vh;
+}
+
+.buyers{
+  margin: auto;
+  width: 50vw;
+  padding: 10px;
+  align-items: center;
+}
+
+.scrollable-list{
+  margin: 4px, 4px;
+  padding: 4px;
+  width: 50vw;
+  height: 50vh;
+  overflow-x: hidden;
+  overflow-y: auto;
+  text-align: center;
+}
+.buyers-table{
+  margin: auto;
+  padding: 10px;
+  align-items: center;
+  border-collapse: collapse;
+}
+
+.buyers-tr, .buyers-th{
+  border: 1px solid #dddddd;
+  text-align: center;
+  padding: 8px;
+}
+
+.buyers-tr:nth-child(even){
+  background-color: #dddddd;
+}
+
+</style>
--- frontend/src/views/SyncData.vue
+++ frontend/src/views/SyncData.vue
@@ -2,55 +2,55 @@
   <div class="top">
     <div class="syncdata">
       <h1>Sincronizar datos</h1>
-        <v-menu
-          ref="menu"
-          v-model="menu"
-          :close-on-content-click="false"
-          :return-value.sync="date"
-          transition="scale-transition"
-          offset-y
-          min-width="auto"
-        >
-          <template v-slot:activator="{ on, attrs }">
-            <v-text-field
-              v-model="date"
-              label="Selecciona la fecha a sincronizar"
-              prepend-icon="mdi-calendar"
-              readonly
-              v-bind="attrs"
-              v-on="on"
-            ></v-text-field>
-          </template>
-          <v-date-picker
+      <v-menu
+        ref="menu"
+        v-model="menu"
+        :close-on-content-click="false"
+        :return-value.sync="date"
+        transition="scale-transition"
+        offset-y
+        min-width="auto"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-text-field
             v-model="date"
-            no-title
-            scrollable
+            label="Selecciona la fecha a sincronizar"
+            prepend-icon="mdi-calendar"
+            readonly
+            v-bind="attrs"
+            v-on="on"
+          ></v-text-field>
+        </template>
+        <v-date-picker
+          v-model="date"
+          no-title
+          scrollable
+        >
+          <v-spacer></v-spacer>
+          <v-btn
+            text
+            color="primary"
+            @click="menu = false"
           >
-            <v-spacer></v-spacer>
-            <v-btn
-              text
-              color="primary"
-              @click="menu = false"
-            >
-              Cancel
-            </v-btn>
-            <v-btn
-              text
-              color="primary"
-              @click="$refs.menu.save(date)"
-            >
-              OK
-            </v-btn>
-          </v-date-picker>
-        </v-menu>
-        <div v-if="!disabled">
-          <v-row aling="center" @click="loadData" :disabled="disabled">
-            <v-btn dark color="blue">Sincronizar</v-btn>
-          </v-row>
-        </div>
-        <div v-else class="loading">
-          Cargando los datos
-        </div>
+            Cancel
+          </v-btn>
+          <v-btn
+            text
+            color="primary"
+            @click="$refs.menu.save(date)"
+          >
+            OK
+          </v-btn>
+        </v-date-picker>
+      </v-menu>
+      <div v-if="!disabled">
+        <v-row aling="center" @click="loadData" :disabled="disabled">
+          <v-btn dark color="blue">Sincronizar</v-btn>
+        </v-row>
+      </div>
+      <div v-else class="loading">
+        Cargando los datos
+      </div>
     </div>
   </div>
 </template>
